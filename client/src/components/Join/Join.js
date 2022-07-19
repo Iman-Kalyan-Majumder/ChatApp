f0a373fa7a04d28fd6ef5a8d9825e8619084ed42
Iman Kalyan Majumder <77956318+Iman-Kalyan-Majumder@users.noreply.github.com>
2022-07-19 23:45:29 +0530

    Update Join.js

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -4,21 +4,21 @@ import {Link} from 'react-router-dom';
 
 import './Join.css';
 
-const Layout=()=>{
+const Join=()=>{
     const [name,setName]=useState("");
     const [room,setRoom]=useState("");
 
     return (
-        <div className="joinWrapper">
-            <div className="join">
+        <div className="joinWrapper col-md-3">
+            <div className="join col-md-12 d-flex align-items-center justify-content-center">
                 <input type="text" placeholder="Name" className="inputField" onChange={(event)=>setName(event.target.value)}/>
-                <input type="text" placeholder="Room" className="inputField mt-20" onChange={(event)=>setRoom(event.target.value)}/>
+                <input type="text" placeholder="Room" className="inputField" onChange={(event)=>setRoom(event.target.value)}/>
                 <Link onClick={(event)=>(!name || !room)? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-                    <button type="submit" className="button mt-20">Sign In</button>
+                    <button type="submit" className="button col-md-12">Sign In</button>
                 </Link>
             </div>
         </div>
     );
 }
 
-export default Layout;
+export default Join;
