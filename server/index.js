b0b6b61271c0caadb5ca8fc91bec4cc40db7f750
Iman Kalyan Majumder <77956318+Iman-Kalyan-Majumder@users.noreply.github.com>
2022-07-19 23:44:02 +0530

    Added Admin Messages

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const http = require('http');
 
 const router = require('./router');
 
-const PORT = process.env.PORT || 5000;//localhost:5000 is used when the server is running locally
+const PORT = process.env.PORT || 5000;
 
 const app=express();
 
@@ -14,7 +14,6 @@ app.use(router);
 
 const server = http.createServer(app);
 
-//without cors, the frontend was not able to connect to the backend.
 const io = socketio(server,{
     cors: {
       origin: '*',
@@ -23,19 +22,29 @@ const io = socketio(server,{
 
 io.on('connection',(socket)=>{
 
-    const {room}=socket.handshake.query;
-    socket.join(room);//user joins a specific room
+    const {name,room}=socket.handshake.query;
 
+    socket.join(room);
+    io.in(room).emit('message',{
+        id: null,
+        name: "Admin",
+        body:`${name} has joined the chat!`
+    });
+    
     socket.on('message',(message)=>{
         io.in(room).emit('message',message);
-    });//upon receiving a message from the frontend, the same message is broadcasted to every user, in the room, by the backend
+    });
 
     socket.on("disconnect", () => {
+        io.in(room).emit('message',{
+            id: null,
+            name: "Admin",
+            body:`${name} has left the chat!`
+        });
         socket.leave(room);
-    });//user leaves the room
+    });
 });
 
-//starting the server ...
 server.listen(PORT,()=>{
     console.log(`Server Listening On Port ${PORT}...`);
 });
